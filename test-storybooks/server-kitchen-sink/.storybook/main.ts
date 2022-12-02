70e4415ea23f0e178b72848131e5a908a8385018
wojtekxtx <wojtekx.tx@gmail.com>
2022-12-02 11:22:07 +0100

    try to fix StoreStoreV7 bug
    https: //github.com/chanzuckerberg/axe-storybook-testing/pull/54/files
    Co-Authored-By: Ruben Oostinga <0xR@users.noreply.github.com>

--- test-storybooks/server-kitchen-sink/.storybook/main.ts
+++ test-storybooks/server-kitchen-sink/.storybook/main.ts
@@ -1,25 +1,25 @@
 import type { StorybookConfig } from '@storybook/server-webpack5';
 
 const mainConfig: StorybookConfig = {
-  stories: ['../stories/**/*.stories.@(json|yaml|yml)'],
-  logLevel: 'debug',
-  addons: [
-    '@storybook/addon-docs',
-    '@storybook/addon-a11y',
-    '@storybook/addon-actions',
-    '@storybook/addon-backgrounds',
-    '@storybook/addon-links',
-    '@storybook/addon-controls',
-    '@storybook/addon-highlight',
-	'@storybook/angular'
-  ],
-  core: {
-    disableTelemetry: true,
-  },
-  features: {
-    storyStoreV7: true,
-  },
-  framework: '@storybook/angular',
+	stories: ["../stories/**/*.stories.@(json|yaml|yml)"],
+	logLevel: "debug",
+	addons: [
+		"@storybook/addon-docs",
+		"@storybook/addon-a11y",
+		"@storybook/addon-actions",
+		"@storybook/addon-backgrounds",
+		"@storybook/addon-links",
+		"@storybook/addon-controls",
+		"@storybook/addon-highlight",
+		"@storybook/angular",
+	],
+	core: {
+		disableTelemetry: true,
+	},
+	features: {
+		storyStoreV7: process.env.NO_STORY_STORE_V7 !== "true",
+	},
+	framework: "@storybook/angular",
 };
 
 module.exports = mainConfig;
