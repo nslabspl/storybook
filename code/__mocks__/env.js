6ed43a90aab3755072a16c9678caecedc2e81672
wojtekxtx <wojtekx.tx@gmail.com>
2022-12-09 17:57:36 +0100

    new mocks
    Co-Authored-By: Ruben Oostinga <0xR@users.noreply.github.com>

--- code/__mocks__/env.js
+++ code/__mocks__/env.js
@@ -1,5 +1,35 @@
-async function isDevEnvMock(){
+export function isDevEnvMock(){
 	if (window.location.pathname.includes === 'dev') {
+		return true
+	}
+}
+
+export function isGeoLocEnabledMock(){
+	if (typeof navigator.geolocation.getCurrentPosition() === "object") {
+		document.getElementById('geoloc_rb').setAttribute('checked');
+	} else {
+		return console.error(GeolocationPositionError);
+	}
+}
+
+export function isBrowserDNTEnabledMock(){
+	if (navigator.doNotTrack) {
+		return true | console.log("DNT enabled");
+	}
+}
+
+export function isHTMLElementPresentMock(htmlTag){
+	htmlTag = "<html>"
+	if (document.querySelector(htmlTag)) {
+		return true
+	} else {
+		let err = Error.toString();
+		return console.log(err);
+	}
+}
+
+export function isLsEnabledMock(){
+	if (localStorage.length !== 0) {
 		return true
 	}
 }
