cdea32ad2fc018d01f6aaf7e36e4bca49b2d1022
wojtekxtx <wojtekx.tx@gmail.com>
2022-12-10 15:40:15 +0100

    [MOCK] check if `devenv` is available from public

--- code/__mocks__/env.js
+++ code/__mocks__/env.js
@@ -1,5 +1,7 @@
 // MIT
 
+import { hasUncaughtExceptionCaptureCallback } from 'process';
+
 export function isDevEnvMock(){
 	if (window.location.pathname.includes === 'dev') {
 		return true
@@ -34,4 +36,18 @@ export function isLsEnabledMock(){
 	if (localStorage.length !== 0) {
 		return true
 	}
+}
+
+export function isDevEnvAccessibleFromPublicMock(){
+	var exec = require('child_process').exec;
+	let resp = exec('ping -c 127.0.0.1');
+
+	if (resp) {
+		document.getElementById('infopanel').style.backgroundColor = 'green';
+		document.getElementById('infopanel').innerHTML += "Env not available from public. Thats good :)";
+	} else {
+		document.getElementById('infopanel').style.backgroundColor = 'red';
+		document.getElementById('infopanel').innerHTML += "Env available from public!. Thats NOT good :(";
+	}
+	return resp;
 }
