285a68155bf09ee91e5462d0308f08d6e8b88228
wojtekxtx <wojtekx.tx@gmail.com>
2022-12-11 08:50:28 +0100

    add function logic

--- code/__mocks__/htmlMock.js
+++ code/__mocks__/htmlMock.js
@@ -1,15 +1,10 @@
 // Returns 1 if page is loaded, 0 if there is an error
-export function isHTMLDocLoadedMock(html){
+function isHTMLDocLoadedMock(html){
 	return Boolean(document.querySelector(html));
 }
 
 // Check if document structure is correct and correctly rendered.
-// This is DRAFT, so not usable!
-// Once completed, just add EXPORT directive and remove middle line comment
 function isDocumentStructuredProperlyAndVisibleMock(){
-	let bodytag = document.getElementById('<body>');
-
-	if (bodytag) {
-		//
-	}
+	let bodytag = document.getElementById('<html>');
+	return (bodytag.offsetParent === null)
 }
