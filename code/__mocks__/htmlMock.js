84fcbcadaa96a78a8abffbe9cfc755ed3861cf5d
wojtekxtx <wojtekx.tx@gmail.com>
2022-12-11 01:11:57 +0100

    structure check mocks
    Co-Authored-By: Ruben Oostinga <0xR@users.noreply.github.com>

--- code/__mocks__/htmlMock.js
+++ code/__mocks__/htmlMock.js
@@ -1,4 +1,15 @@
 // Returns 1 if page is loaded, 0 if there is an error
-export function isHTMLDocLoaded(html){
+export function isHTMLDocLoadedMock(html){
 	return Boolean(document.querySelector(html));
+}
+
+// Check if document structure is correct and correctly rendered.
+// This is DRAFT, so not usable!
+// Once completed, just add EXPORT directive and remove middle line comment
+function isDocumentStructuredProperlyAndVisibleMock(){
+	let bodytag = document.getElementById('<body>');
+
+	if (bodytag) {
+		//
+	}
 }
