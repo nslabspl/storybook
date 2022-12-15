3dcbdd632c24e817629ebb94edaa086f567d19c8
wojtekxtx <wojtekx.tx@gmail.com>
2022-12-15 10:40:37 +0100

    export tags to devConf

--- code/dev-config.js
+++ code/dev-config.js
@@ -1,6 +1,11 @@
 const devSrvAddress = "127.0.0.1:80";
+const devMainLandingPage = "/main.php";
 const devEnvName = "StoryBook dev env";
 const devEnvCharset = "utf-8";
 const isContainerized = true;
 
-export { devEnvName, devSrvAddress, devEnvCharset, isContainerized };
+// Tags etc
+const devDesiredTags = ["<html>", "<head>", "<title>", "<body>"];
+
+
+export { devEnvName, devSrvAddress, devEnvCharset, isContainerized, devMainLandingPage, devDesiredTags };
