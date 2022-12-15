e5d092c60a558cb588e78ef354c445072d548f67
wojtekxtx <wojtekx.tx@gmail.com>
2022-12-15 09:12:23 +0100

    Create dev-config.js

---
+++ code/dev-config.js
@@ -0,0 +1,6 @@
+const devSrvAddress = "127.0.0.1:80";
+const devEnvName = "StoryBook dev env";
+const devEnvCharset = "utf-8";
+const isContainerized = true;
+
+export { devEnvName, devSrvAddress, devEnvCharset, isContainerized };
