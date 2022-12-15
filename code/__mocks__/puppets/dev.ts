73da6b629a34e528e9d8d0151d04110950439bde
wojtekxtx <wojtekx.tx@gmail.com>
2022-12-15 09:12:09 +0100

    Start/Stop devServer

---
+++ code/__mocks__/puppets/dev.ts
@@ -0,0 +1,21 @@
+// Source: https://github.com/cypress-io/cypress/blob/develop/npm/webpack-dev-server/src/devServer.ts#L63
+export function devServer (devServerConfig: WebpackDevServerConfig): Promise<ResolvedDevServerConfig> {
+  return new Promise(async (resolve, reject) => {
+    const result = await devServer.create(devServerConfig) as DevServerCreateResult
+
+    if (result.version === 3) {
+      const srv = result.server.listen(0, '127.0.0.1', () => {
+        const port = (srv.address() as AddressInfo).port
+        resolve({port, close: (done) => {
+            srv.close((err) => {
+              if (err) {
+                debug('closing dev server, with error', err)
+              }
+              debug('closed dev server')
+              done?.(err)
+            })
+          },
+        })
+      })
+      return devServerConfig
+    }
